e402b0df8544fad3d40e1a58519c61bda6a392f9
Dario M. Mannu <d.mannu@cubelets.digital>
2024-01-29 22:56:02 +0000

    fix types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
+/**
+ * A JSON entity
+ */
+export type JSON = string | number | boolean | null | JSONObject | JSON[];
+/**
+ * A JSON object
+ */
+interface JSONObject {
+  [property: string]: JSON;
+};
+
 export type Topic = string & { _Topic: never };
 
 export type SingleHandler<InputType, OutputType> = (data: InputType) => OutputType;
@@ -24,19 +35,17 @@ export type IPlugMessageBus = SerialCall<unknown, unknown> & {
 	parallel: ParallelCall<unknown, unknown>;
 };
 export type iPlug = IPlugMessageBus;
-export type PluginModule = PluginManifest | ((plugins: iPlug, config?: Config) => Promise<PluginManifest>);
-
+export type PluginModule = PluginManifest | ((plugins: iPlug, config?: IPlugConfig) => Promise<PluginManifest>);
 export type PluginName = string & { _PluginName: never };
 
-
 export type TopicRegistry = Map<Topic, Handler[]>;
-export type _HookRegistry = TopicRegistry | Record<Topic, Handler[]>;
 
-export type ModuleName = string;
-export type ModuleConfig = unknown;
-export type Config = (Record<ModuleName, ModuleConfig> | unknown) & { _Config: never };
+export type ModuleSubConfig = JSONObject;
+export type ModuleMainConfig = Record<PluginName, ModuleSubConfig>;
+export type ModuleConfig = ModuleMainConfig | ModuleSubConfig & { _ModuleConfig: never };
+export type IPlugConfig = (Record<PluginName, ModuleConfig> | Record<string, unknown> | string | number | boolean) & { _Config: never };
 
-export default async function iPlug(modules: PluginManifest, config: Config): Promise<IPlugMessageBus> {
+export default async function iPlug(modules: PluginManifest, config: IPlugConfig): Promise<IPlugMessageBus> {
 	const notReady = (): never => {	throw new Error('plugins/messagebus unavailable at') };
 
 	const identity: Handler = x => x;
@@ -46,25 +55,25 @@ export default async function iPlug(modules: PluginManifest, config: Config): Pr
 
 	const messagebus = <IPlugMessageBus>serial;
 	messagebus.serial = serial;
-	messagebus.one = (msg, initialData) => getHooks(msg, [identity]).slice(0, 1).reduce(serial, initialData);
+	messagebus.one = (msg: Topic, initialData: unknown) => getHooks(msg, [identity]).slice(0, 1).reduce(callPlugin, initialData);
 	messagebus.parallel = <I, O>(msg: Topic, seedData: I) => <O>getHooks(msg, <Handler[]>[]).map(function parallel(plugin){return <I>plugin(seedData)});
 	messagebus.init = () => messagebus;
 
 	const kvp = <[PluginName, PluginModule][]>Object.entries(modules ?? <PluginModule>{});
 
 	const initModule = (name: PluginName, module: PluginModule): PluginManifest | Promise<PluginManifest> =>
-		isPluginManifest(module) ? <PluginManifest>module : module(messagebus, config?.hasOwnProperty(name) ? config[name] : config)
+		isPluginManifest(module)
+			? <PluginManifest>module
+			: module(messagebus, (config.hasOwnProperty(name) ? <IPlugConfig>(<ModuleMainConfig>config)[name] : config));
 
 	const initModules = async ([name, module]: [PluginName, PluginModule]): Promise<[PluginName, PluginManifest]> =>
 		[name, await initModule(name, module)];
 
-	const _hm = <Handler[]>[];
-	const mergeHooks = (handlers?: Handler[], h: Handler): Handler[] => _hm.concat(handlers ?? _hm, h ?? _hm);
+	const _h = <Handler[]>[];
+	const mergeHooks = (handlers: Handler[] | undefined, h: Handler): Handler[] => _h.concat(handlers ?? _h, h ?? _h);
 	const pluginModules = new Map(await Promise.all(kvp.map(initModules)));
 	const missingPlugin = (pluginName: PluginName): PluginManifest => ({[pluginName]: ()=>{throw new Error(`plugin ${pluginName} is missing`)}});
 
-	debugger;
-	const _pm = <PluginModule[]>[];
 	const hooks: TopicRegistry =
 		([...pluginModules.entries()])
 		// for each plugin
@@ -72,15 +81,9 @@ export default async function iPlug(modules: PluginManifest, config: Config): Pr
 			// look all hooks up (plugin modules)
 			.entries(pluginManifest ?? missingPlugin(pluginName))
 			// and register them for later lookup and use
-			.reduce((hooks: TopicRegistry, [topic, next]) => {
-
-				debugger;
-				const b = mergeHooks(hooks.get(<Topic>topic), next);
-				const a = (hooks.set(<Topic>topic, b), hooks)
-				return a
-			
-			}
-				, hooks)
+			.reduce((hooks: TopicRegistry, [topic, next]) =>
+				(hooks.set(<Topic>topic, mergeHooks(hooks.get(<Topic>topic), next)), hooks)
+			, hooks)
 		, <TopicRegistry>new Map())
 	;
 
